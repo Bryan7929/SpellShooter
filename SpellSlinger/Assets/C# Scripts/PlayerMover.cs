b2ae13a0cc10f86ffd3fd7fbf6b9e0ad5baeaa98
Bryan Hernandez <117753383+Bryan7929@users.noreply.github.com>
2025-02-09 20:46:46 -0600

    WIP Player Movement
    Made edits to player movement to only move on a x-axis or y-axis at a time. Still needs work.
    2/9/2025

--- SpellSlinger/Assets/C# Scripts/PlayerMover.cs
+++ SpellSlinger/Assets/C# Scripts/PlayerMover.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using JetBrains.Annotations;
 
 public class PlayerMover : MonoBehaviour
 {
     public float movSpeed;
     float speedX, speedY;
+
+    bool movingY = false;
+    bool movingX = false;
+
     Rigidbody2D rb;
+
 
     void Start()
     {
@@ -15,8 +22,51 @@ public class PlayerMover : MonoBehaviour
 
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)||Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(movingX)
+            {
+                speedY = 0;
+                movingX = true;
+            }
+            speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
+            rb.linearVelocity = new Vector2(speedX, speedY);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)||Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(movingY)
+            {
+                speedX = 0;
+                movingY = true;
+            }
+            speedY = Input.GetAxisRaw("Vertical") * movSpeed;
+            rb.linearVelocity = new Vector2(speedX, speedY);
+        }
+        if(Input.GetKeyUp(KeyCode.LeftArrow)||Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            movingX = false;
+            speedX = 0;
+            rb.linearVelocity = new Vector2(speedX, speedY);
+        }
+        if(Input.GetKeyUp(KeyCode.UpArrow)||Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            movingY = false;
+            speedY = 0;
+            rb.linearVelocity = new Vector2(speedX, speedY);
+        }
+ 
+    }
+}
+
+
+
+
+
+
+        /*
         speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
         speedY = Input.GetAxisRaw("Vertical") * movSpeed;
         rb.linearVelocity = new Vector2(speedX, speedY);
-    }
-}
+        */
